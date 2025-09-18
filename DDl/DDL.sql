f47623adb3aec5b5782590aab671763af26c0945
Franciele <francielej2744@gmail.com>
2025-09-18 20:52:07 -0300

    Add files via upload

---
+++ DDl/DDL.sql
@@ -0,0 +1,10 @@
+USE db_aula_pk_fk;
+INSERT INTO
+cursos (nome, descricao)
+VALUES
+('Desenvolvimento de sistemas', 'Curso voltado para desenvolvimento de software e boas praticas.'),
+('Administracao', 'curso sobre analise e administracao de empresas.'),
+('Nutricao', 'Curso sobre neutrologia, cadeia alimentar e saude humana.');
+
+SELECT * FROM cursos; 
+
